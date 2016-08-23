9a5b8c57160ed29456f938d23d3ca7274ba0fa66
Sachin Karia <sachin.karia01@gmail.com>
2016-08-23 16:34:41 +0100

    added mark completed function which deletes specific todo from list

--- mainController.js
+++ mainController.js
@@ -4,6 +4,13 @@ app.controller('mainController', ['$scope',function($scope){
   $scope.currentData = Date.now();
 
   $scope.addTodo = function(todo){
-    $scope.todoList.push({description: $scope.descriptionText, created: Date.now()})
+    $scope.todoList.push({description: $scope.descriptionText, created: Date.now()});
   }
+
+  $scope.removeTodo = function(todo){
+    var index = $scope.todoList.indexOf(todo)
+    if(index > -1){
+      $scope.todoList.splice(index,1);
+    };
+  };
 }]);
