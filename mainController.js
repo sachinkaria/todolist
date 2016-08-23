21809f423d37a0f2fe9f291d38e0f5da8c1ab620
Sachin Karia <sachin.karia01@gmail.com>
2016-08-23 14:32:38 +0100

    added addTodo function with unformatted date variable

--- mainController.js
+++ mainController.js
@@ -1,4 +1,9 @@
 app.controller('mainController', ['$scope',function($scope){
   $scope.test = 'Hello world!';
   $scope.todoList = [{description: "First To Do", created: "23/08/2016"}];
+  $scope.currentData = Date.now();
+
+  $scope.addTodo = function(todo){
+    $scope.todoList.push({description: $scope.descriptionText, created: Date.now()})
+  }
 }]);
