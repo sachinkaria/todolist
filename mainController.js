c4f483ceb983b7c38533f8a3fae2dbd013725686
Sachin Karia <sachin.karia01@gmail.com>
2016-08-24 10:35:41 +0100

    added working edit todo functionality

--- mainController.js
+++ mainController.js
@@ -2,11 +2,12 @@ app.controller('mainController', ['$scope',function($scope){
   $scope.test = 'Hello world!';
   $scope.todoList = [];
   $scope.currentData = Date.now();
+  $scope.editable = false;
 
   $scope.addTodo = function(todo){
     $scope.todoList.push({description: $scope.descriptionText, created: Date.now()});
     $scope.descriptionText = '';
-  }
+  };
 
   $scope.removeTodo = function(todo){
     var index = $scope.todoList.indexOf(todo)
@@ -14,4 +15,14 @@ app.controller('mainController', ['$scope',function($scope){
       $scope.todoList.splice(index,1);
     };
   };
+
+  $scope.editTodo = function(todo){
+    $scope.editable = true;
+    $scope.existingTodo = todo;
+  };
+
+  $scope.saveTodo = function(){
+    $scope.existingTodo = {};
+    $scope.editable = false;
+  }
 }]);
